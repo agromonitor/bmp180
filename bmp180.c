a09a8a71fb59a6646edd7fa34bf717ffe4e428fb
Konstantinos Tsimpoukas <kostaslinuxxx@gmail.com>
2015-11-15 13:03:25 -0600

    Second Patch

--- bmp180.c
+++ bmp180.c
@@ -13,7 +13,7 @@ int main(){
        //Open a file to I2C Bus 3 and tell it that there is a BMP085 located at 0x77.
        snprintf(path,sizeof path, "/sys/class/i2c-adapter/i2c-1/new_device");
 	if((fp = fopen(path,"w")) == NULL){
-		printf("file open failed");
+		printf("file open failed!");
 		return 1;
 	        }
        rewind(fp);
@@ -26,12 +26,12 @@ int main(){
               //Attempt to open the file of the device.
               snprintf(path, sizeof path, "/sys/bus/i2c/drivers/bmp085/1-0077/temp0_input");
               if((fp = fopen(path, "r")) == NULL){
-	            printf("cannot open device file");
+	            printf("cannot open device file!");
 	            return 1;
 	            }
               //Attempt to read the temperature from the device.
               if((fgets(buf, MAX_BUF, fp)) == NULL){
-	            printf("cannot read device");
+	            printf("cannot read device!");
 	           }
               fclose(fp);
               float temp = atoi(buf);
@@ -40,7 +40,7 @@ int main(){
              //Attempt to open the file of the device.
              snprintf(path, sizeof path, "/sys/bus/i2c/drivers/bmp085/1-0077/pressure0_input");
              if((fp = fopen(path, "r")) == NULL){
-	          printf("cannot read");
+	          printf("cannot read!");
 	          return 1;
 	          }
              //Attempt to read the pressure from the device.
